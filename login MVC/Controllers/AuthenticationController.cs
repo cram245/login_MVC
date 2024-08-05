d1827d56a34d4a8cef0a38a91cfecb4442b36c34
cram245 <marcalbaf3@gmail.com>
2024-08-05 12:42:01 +0200

    Agregar archivos de proyecto.

---
+++ login MVC/Controllers/AuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using login_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+
+namespace login_MVC.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(loginViewModel model)
+        {
+
+            if (ModelState.IsValid)
+                return RedirectToAction("Index", "Home");
+
+            ViewBag.Error = "Ha fallado el login";
+            return View(model);
+        }
+    }
+}
---
+++ login MVC/Models/loginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace login_MVC.Models
+{
+    public class loginViewModel
+    {
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+    }
+}
